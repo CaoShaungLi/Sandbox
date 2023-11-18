19b303966be4c4007866aa4484549c19593a0c4d
Cao Shaung Li <caoshaungli@my.jcu.edu.au>
2023-11-18 20:22:15 +0800

    Update and debug password_stars.py

--- password_stars.py
+++ password_stars.py
@@ -2,4 +2,5 @@ MINIMUM_PASSWORD_LENGTH = 10
 password = input("Enter password: ")
 while len(password) < MINIMUM_PASSWORD_LENGTH:
     print("Invalid password, should be at least 10 characters")
+    password = input("Enter password: ")
 print("Password: ", "*"*len(password))
