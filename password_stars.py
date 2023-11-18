c30f26613d204cf6b283016c7d69ba274f6c8f13
Cao Shaung Li <caoshaungli@my.jcu.edu.au>
2023-11-18 17:03:42 +0800

    Add password check program

--- password_stars.py
+++ password_stars.py
@@ -1,3 +1,5 @@
-"""
-
-"""
+MINIMUM_PASSWORD_LENGTH = 10
+password = input("Enter password: ")
+while len(password) < MINIMUM_PASSWORD_LENGTH:
+    print("Invalid password, should be at least 10 characters")
+print("Password: ", "*"*len(password))
